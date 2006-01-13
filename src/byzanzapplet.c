665d5a8e88e17a5138d5f31fdfcd144aeb303e11
Benjamin Otte <otte@src.gnome.org>
2006-01-13 00:04:50 +0000

    tweak string displayed and translate it.
    * po/de.po:
    * src/byzanzapplet.c: (byzanz_applet_stop_recording):
    tweak string displayed and translate it.

--- src/byzanzapplet.c
+++ src/byzanzapplet.c
@@ -24,10 +24,150 @@
 #include <panel-applet.h>
 #include <gtk/gtklabel.h>
 #include <glib/gstdio.h>
+#include <libgnomevfs/gnome-vfs.h>
+#include <libgnomeui/libgnomeui.h>
 #include "byzanzrecorder.h"
 #include "byzanzselect.h"
 #include "panelstuffer.h"
 #include "i18n.h"
+
+/*** PENDING RECORDING ***/
+
+typedef struct {
+  ByzanzRecorder *	rec;
+  GnomeVFSAsyncHandle *	handle;
+  char *		tmp_file;
+  GnomeVFSURI *		target;
+} PendingRecording;
+
+static void
+byzanz_applet_show_error (GtkWindow *parent, const char *error, const char *details, ...)
+{
+  GtkWidget *dialog;
+  gchar *msg;
+  va_list args;
+
+  g_return_if_fail (details != NULL);
+  
+  va_start (args, details);
+  msg = g_strdup_vprintf (details, args);
+  va_end (args);
+  dialog = gtk_message_dialog_new (parent, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR,
+      GTK_BUTTONS_CLOSE, error ? error : msg);
+  if (error)
+    gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (dialog), msg);
+  gtk_dialog_run (GTK_DIALOG (dialog));
+  gtk_widget_destroy (dialog);
+  g_free (msg);
+}
+
+static void
+pending_recording_destroy (PendingRecording *pending)
+{
+  g_assert (pending->rec == NULL);
+
+  if (pending->tmp_file) {
+    g_unlink (pending->tmp_file);
+    g_free (pending->tmp_file);
+  }
+  gnome_vfs_uri_unref (pending->target);
+  g_free (pending);
+  gtk_main_quit ();
+}
+
+static int
+transfer_progress_cb (GnomeVFSAsyncHandle *handle, GnomeVFSXferProgressInfo *info, gpointer data)
+{
+  PendingRecording *pending = data;
+  char *target_uri;
+
+  switch (info->status) {
+    case GNOME_VFS_XFER_PROGRESS_STATUS_VFSERROR:
+      target_uri = gnome_vfs_uri_to_string (pending->target, GNOME_VFS_URI_HIDE_PASSWORD | GNOME_VFS_URI_HIDE_FRAGMENT_IDENTIFIER);
+      byzanz_applet_show_error (NULL, _("A file could not be saved."),
+	  _("\"%s\" could not be saved.\nThe error that occured was: %s"), 
+	    target_uri, gnome_vfs_result_to_string (info->vfs_status));
+      g_free (target_uri);
+      return 0;
+    case GNOME_VFS_XFER_PROGRESS_STATUS_OK:
+      if (info->phase == GNOME_VFS_XFER_PHASE_COMPLETED) {
+	g_free (pending->tmp_file);
+	pending->tmp_file = NULL;
+	pending_recording_destroy (pending);
+	gtk_main_quit ();
+	return 0;
+      }
+      return 1;
+    case GNOME_VFS_XFER_PROGRESS_STATUS_OVERWRITE:
+    case GNOME_VFS_XFER_PROGRESS_STATUS_DUPLICATE:
+    default:
+      g_assert_not_reached ();
+      return 0;
+  }
+}
+
+static gboolean
+check_done_saving_cb (gpointer data)
+{
+  PendingRecording *pending = data;
+  GList *src, *target;
+  GnomeVFSURI *src_uri;
+
+  if (byzanz_recorder_is_active (pending->rec))
+    return TRUE;
+  byzanz_recorder_destroy (pending->rec);
+  pending->rec = NULL;
+
+  if (pending->target == NULL) {
+    pending_recording_destroy (pending);
+    return FALSE;
+  }
+  src_uri = gnome_vfs_uri_new (pending->tmp_file);
+  src = g_list_prepend (NULL, src_uri);
+  target = g_list_prepend (NULL, pending->target);
+  gnome_vfs_async_xfer (&pending->handle, src, target,
+      GNOME_VFS_XFER_REMOVESOURCE | GNOME_VFS_XFER_TARGET_DEFAULT_PERMS,
+      GNOME_VFS_XFER_ERROR_MODE_QUERY,
+      GNOME_VFS_XFER_OVERWRITE_MODE_REPLACE, /* we do overwrite confirmation in the dialog */
+      GNOME_VFS_PRIORITY_DEFAULT,
+      transfer_progress_cb, pending, NULL, NULL);
+
+  g_list_free (src);
+  g_list_free (target);
+  gnome_vfs_uri_unref (src_uri);
+  return FALSE;
+}
+
+static void
+pending_recording_launch (ByzanzRecorder *rec, char *tmp_file)
+{
+  PendingRecording *pending;
+  GtkWidget *dialog;
+  
+  pending = g_new0 (PendingRecording, 1);
+  pending->rec = rec;
+  pending->tmp_file = tmp_file;
+  
+  dialog = gtk_file_chooser_dialog_new (_("Save Recorded File"),
+      NULL, GTK_FILE_CHOOSER_ACTION_SAVE,
+      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+      GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
+      NULL);
+  gtk_file_chooser_set_local_only (GTK_FILE_CHOOSER (dialog), FALSE);
+  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), g_get_home_dir ());
+  gtk_file_chooser_set_do_overwrite_confirmation (GTK_FILE_CHOOSER (dialog), TRUE);
+  gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_ACCEPT);
+  if (GTK_RESPONSE_ACCEPT == gtk_dialog_run (GTK_DIALOG (dialog))) {
+    char *target_uri = gtk_file_chooser_get_uri (GTK_FILE_CHOOSER (dialog));
+    pending->target = gnome_vfs_uri_new (target_uri);
+  }
+  gtk_widget_destroy (dialog);
+  g_timeout_add (1000, check_done_saving_cb, pending);
+  gtk_main ();
+  return;
+}
+
+/*** APPLET ***/
 
 typedef struct {
   PanelApplet *		applet;		/* the applet we manage */
@@ -42,31 +182,6 @@ typedef struct {
   guint			update_func;	/* id of idle function that updates state */
 } AppletPrivate;
 #define APPLET_IS_RECORDING(applet) ((applet)->tmp_file != NULL)
-
-static void
-byzanz_applet_show_error (AppletPrivate *priv, GtkWindow *parent, const char *error)
-{
-  GtkWidget *dialog;
-
-  if (parent == NULL)
-    parent = GTK_WINDOW (gtk_widget_get_toplevel (GTK_WIDGET (priv->applet)));
-  dialog = gtk_message_dialog_new (parent, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR,
-      GTK_BUTTONS_CLOSE, error);
-  gtk_dialog_run (GTK_DIALOG (dialog));
-  gtk_widget_destroy (dialog);
-}
-
-static void
-byzanz_applet_destroy_recorder (AppletPrivate *priv)
-{
-  g_assert (priv->rec);
-
-  byzanz_recorder_destroy (priv->rec);
-  priv->rec = NULL;
-  gtk_label_set_text (GTK_LABEL (priv->label), _("OFF"));
-  g_source_remove (priv->update_func);
-  priv->update_func = 0;
-}
 
 static gboolean
 byzanz_applet_is_recording (AppletPrivate *priv)
@@ -107,17 +222,9 @@ byzanz_applet_update (gpointer data)
     gtk_widget_set_sensitive (priv->button, TRUE);
   } else {
     gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->button), FALSE);
-    if (priv->rec && !byzanz_recorder_is_active (priv->rec))
-      byzanz_applet_destroy_recorder (priv);
-    if (priv->rec) {
-      /* applet is not recording, but still saving content */
-      byzanz_applet_ensure_text (priv, _("SAVE"));
-      gtk_widget_set_sensitive (priv->button, FALSE);
-    } else {
-      /* applet is idle */
-      byzanz_applet_ensure_text (priv, _("OFF"));
-      gtk_widget_set_sensitive (priv->button, TRUE);
-    }
+    gtk_label_set_text (GTK_LABEL (priv->label), _("OFF"));
+    g_source_remove (priv->update_func);
+    priv->update_func = 0;
   }
   
   return TRUE;
@@ -160,42 +267,18 @@ out:
 static void
 byzanz_applet_stop_recording (AppletPrivate *priv)
 {
-  GtkWidget *dialog;
   char *tmp_file;
+  ByzanzRecorder *rec;
   
   g_assert (byzanz_applet_is_recording (priv));
   
   byzanz_recorder_stop (priv->rec);
   tmp_file = priv->tmp_file;
   priv->tmp_file = NULL;
+  rec = priv->rec;
+  priv->rec = NULL;
   byzanz_applet_update (priv);
-  dialog = gtk_file_chooser_dialog_new (_("Save Recorded File"),
-      GTK_WINDOW (gtk_widget_get_toplevel (GTK_WIDGET (priv->applet))), 
-      GTK_FILE_CHOOSER_ACTION_SAVE,
-      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-      GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
-      NULL);
-  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), g_get_home_dir ());
-  gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_ACCEPT);
-repeat:
-  if (GTK_RESPONSE_ACCEPT == gtk_dialog_run (GTK_DIALOG (dialog))) {
-    gchar *filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
-    errno = 0;
-    if (g_rename (tmp_file, filename) != 0) {
-      int save_errno = errno;
-      char *display_name = g_filename_display_name (filename);
-      char *error = g_strdup_printf (_("The file could not be saved to %s.\n"
-	    "The error given was: %s.\n"
-	    "Please try a different file."), display_name, g_strerror (save_errno));
-
-      byzanz_applet_show_error (priv, GTK_WINDOW (dialog), error);
-      goto repeat;
-    }
-    g_free (filename);
-  } else {
-    g_unlink (tmp_file);
-  }
-  gtk_widget_destroy (dialog);
+  pending_recording_launch (rec, tmp_file);
 }
 
 static void
@@ -215,8 +298,7 @@ destroy_applet (GtkWidget *widget, AppletPrivate *priv)
 {
   if (byzanz_applet_is_recording (priv))
     byzanz_applet_stop_recording (priv);
-  if (priv->rec) 
-    byzanz_applet_destroy_recorder (priv);
+  g_assert (!priv->rec); 
   g_free (priv);
 }
 
@@ -226,6 +308,9 @@ byzanz_applet_fill (PanelApplet *applet, const gchar *iid, gpointer data)
   AppletPrivate *priv;
   GtkWidget *image, *stuffer;
   
+  gnome_vfs_init ();
+  gnome_authentication_manager_init ();
+
   priv = g_new0 (AppletPrivate, 1);
   priv->applet = applet;
   g_signal_connect (applet, "destroy", G_CALLBACK (destroy_applet), priv);
