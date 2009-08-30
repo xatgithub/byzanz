de552c5c0fc3d1c7174537d869da8ddcb67acdf9
Benjamin Otte <otte@gnome.org>
2009-08-30 21:24:02 +0200

    Get rid of outdated macro magic
    Use current magic instead

--- src/record.c
+++ src/record.c
@@ -121,7 +121,7 @@ main (int argc, char **argv)
   
   g_set_prgname (argv[0]);
 #ifdef GETTEXT_PACKAGE
-  bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+  bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
 #endif
