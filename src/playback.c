9757395137694c8f2b2c373199afd600739cd8b7
Benjamin Otte <otte@redhat.com>
2010-02-20 11:29:28 +0100

    Upadte man pages to 0.2.1

--- src/playback.c
+++ src/playback.c
@@ -74,7 +74,7 @@ main (int argc, char **argv)
   g_thread_init (NULL);
   g_type_init ();
 
-  context = g_option_context_new (_("Process a byzanz debug file"));
+  context = g_option_context_new (_("process a byzanz debug recording"));
 #ifdef GETTEXT_PACKAGE
   g_option_context_set_translation_domain(context, GETTEXT_PACKAGE);
 #endif
